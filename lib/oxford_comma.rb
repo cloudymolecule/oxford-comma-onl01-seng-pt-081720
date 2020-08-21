8d804bb2ea7b7e8957883b678e37322bc38a8d0f
Dario Carlino <baires82@gmail.com>
2020-08-21 01:14:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    array[0..1].join(", ")
-
+    new_array = array[0..1].join(", ")
+    new_array << ", and starfruit"
   end
 end
